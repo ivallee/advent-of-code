533c842244688fbe671e9adede19f5160633b99b
ivallee <isaacvallee@gmail.com>
2017-12-01 15:45:43 -0800

    write sum function

--- 1.js
+++ 1.js
@@ -12,14 +12,13 @@ const numbers = input.split('').map(i => parseInt(i, 10));
 // checks if numbers match next number
 function checkSequence(arr) {
   const output = [];
-  const last = arr[arr.length-1];
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i + 1]) {
       output.push(arr[i]);
     }
   }
-  if (arr[0] === last) {
+  if (arr[0] === arr[arr.length-1]) {
     output.push(arr[0]);
   }
   return output;
