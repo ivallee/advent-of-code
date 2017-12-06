0d8cad74ec13cc102e9866b1d67eda6897c10c7d
ivallee <isaacvallee@gmail.com>
2017-12-05 17:08:25 -0800

    init commit. determine spiral cycles

---
+++ d3-1.js
@@ -0,0 +1,16 @@
+function spiral(n) {
+
+  let x = 0;
+  let y = 0;
+  let direction = 'EAST';
+  let square = Math.pow(1, 2);
+
+  const cycles = Math.floor((Math.sqrt(n + 1) - 1) / 2) + 1;
+
+console.log('Spiral Cycles :', cycles);
+
+}
+
+spiral(400);
+
+// 
