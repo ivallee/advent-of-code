578fc97a5c2e40fa9ab7ca7268b45062d42e45a1
ivallee <isaacvallee@gmail.com>
2017-12-05 17:10:25 -0800

    determine number of spiral cycles

--- d3-1.js
+++ d3-1.js
@@ -3,7 +3,7 @@ function spiral(n) {
   let x = 0;
   let y = 0;
   let direction = 'EAST';
-  let square = Math.pow(1, 2);
+  let square = Math.pow(1, 1);
 
   const cycles = Math.floor((Math.sqrt(n + 1) - 1) / 2) + 1;
 
