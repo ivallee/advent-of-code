aae5d1c7b9dd61f1182d3b09468314cdf24685b1
ivallee <isaacvallee@gmail.com>
2017-12-05 17:20:07 -0800

    determine grid width

--- d3-1.js
+++ d3-1.js
@@ -3,11 +3,13 @@ function spiral(n) {
   let x = 0;
   let y = 0;
   let direction = 'EAST';
-  let square = Math.pow(1, 1);
-
+  let square = 3;
+  let gridWidth = Math.pow(square, 2) / square;
   const cycles = Math.floor((Math.sqrt(n + 1) - 1) / 2) + 1;
 
+
 console.log('Spiral Cycles :', cycles);
+console.log('grid width: ', gridWidth)
 
 }
 
