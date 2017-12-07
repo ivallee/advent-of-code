191d295b124e9ebeb3efaa574462a4c14e303b7f
ivallee <isaacvallee@gmail.com>
2017-12-07 13:16:36 -0800

    function to check if arrays equal

--- d6-1.js
+++ d6-1.js
@@ -1,21 +1,45 @@
 const test = [0, 2, 7, 0];
+let cycles = 0;
+let last = [];
 const batches = [];
 
 const redistribute = (arr) => {
   
   let max = Math.max(...arr);
   const maxIndex = arr.indexOf(max);
-  arr[maxIndex] = 0;
+  const output = [...arr];
+  output[maxIndex] = 0;
 
   for (let i = maxIndex + 1; max > 0; i++) {
-    max -= 1;
-    arr[i] += 1;
     if  (i === arr.length) {
       i = 0;
     }
-    return arr;
+    max -= 1;
+    output[i] += 1;
   }
+  return output;
 };
 
-batches.push(redistribute(test));
+const arraysEqual = (arr1, arr2) => {
+  if(arr1.length !== arr2.length)
+      return false;
+  for(var i = arr1.length; i--;) {
+      if(arr1[i] !== arr2[i])
+          return false;
+  }
+  return true;
+};
+
+last = redistribute(test)
+console.log(last)
+batches.push(last)
+
+prev = redistribute(last)
+console.log(prev)
+batches.push(prev)
 console.log(batches);
+
+
+
+
+// check if 'last' is same as any in array
