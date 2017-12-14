521dc4751e73b9a588c9a9772134d1acb24e4453
ivallee <isaacvallee@gmail.com>
2017-12-14 13:09:56 -0800

    solved part 1 messily

---
+++ d8-1.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const input = fs.readFileSync("./d8-input.txt").toString('utf-8').split("\n");
+
+const testInput = [
+  'b inc 5 if a > 1',
+  'a inc 1 if b < 5',
+  'c dec -10 if a >= 1',
+  'c inc -20 if c == 10',
+]
+
+
+
+const solve = (arr) => {
+  const registers = {};
+
+  for (let i = 0; i < arr.length; ++i) {
+    const instruction = arr[i].split(" ");
+    const registerA = instruction[0];
+    const registerB = instruction[4];
+    const direction = instruction[1] == 'inc' ? '+' : '-';
+    const operator = instruction[5];
+    const compareTo = instruction[6];
+    const change = parseInt(instruction[2]);
+    if (!registers.hasOwnProperty(registerA)) {
+      registers[registerA] = 0;
+    }
+    if (!registers.hasOwnProperty(registerB)) {
+      registers[registerB] = 0;
+    }
+    if (eval(registers[registerB] + operator + compareTo)) {
+      registers[registerA] = eval(registers[registerA] + direction + " " + (change));
+    }
+  }
+  const objArr = Object.keys(registers).map(function (key) { return registers[key]; });
+  return Math.max(...objArr);
+};
+
+console.log(solve(input));
