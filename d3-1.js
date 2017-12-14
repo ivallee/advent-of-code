4085f4c204ae91c43b32b1025c2755cdb0f749b0
ivallee <isaacvallee@gmail.com>
2017-12-14 14:00:47 -0800

    cleanup

--- d3-1.js
+++ d3-1.js
@@ -1,89 +1,3 @@
-// // locations
-// const loc = {
-//   x: 0,
-//   y: 0,
-//   direction: 'EAST'
-// }
-
-
-// // movement
-// let steps = 1;
-// let side = 0;
-
-// const turn = (sideLength) => {
-//   if (side === 0 ) {
-//     sideLength = 1;
-//   }
-//   switch(loc.direction) {
-//    case('NORTH'):
-//     loc.y += sideLength;
-//     steps += sideLength;
-//     loc.direction = 'WEST';
-//     break;
-//   case('WEST'):
-//     loc.x -= sideLength;
-//     steps += sideLength;
-//     loc.direction = 'SOUTH';
-//     break;
-//   case('SOUTH'):
-//     loc.y -= sideLength;
-//     steps += sideLength;
-//     loc.direction = 'EAST';
-//     break;
-//   case('EAST'):
-//     loc.x += sideLength;
-//     steps += sideLength;
-//     loc.direction = 'NORTH';
-//     break;
-//   default:
-//     break;
-//   }
-// };
-
-// const move = (place) => {
-//   while (place > steps) {
-//     turn(side);
-//     // steps += side;
-//     console.log(loc)
-//     turn(side);
-//     // steps += side;
-//     side++;
-//     console.log(steps)
-//     console.log(loc)
-//     console.log('side', side)
-//   }
-// };
-
-// move(9);
-
-// function spiral(n) {
-
-//   let x = 0;
-//   let y = 0;
-//   let direction = 'EAST';
-//   let square = 1;
-//   let gridWidth = Math.pow(square, 2) / square;
-//   const cycles = Math.floor((Math.sqrt(n + 1) - 1) / 2) + 1;
-
-
-// console.log('Spiral Cycles :', cycles);
-// console.log('grid width: ', gridWidth)
-
-// }
-
-// spiral(400);
-
-// if total number is equal to grid width
-  // make a new row and turn
-
-
-// PATTERN
-// move once
-// move, turn, move, turn
-//
-
-
-   
 const input = 312051;
  
 let i = 0;
@@ -92,7 +6,6 @@ let sum = 1;
 let lenghtOfSide = 0;
 let lengthOfRing = 0;
  
-//find the correct ring the number is one
 while(sum < input){
     ring++;
     i++;
@@ -107,10 +20,8 @@ let coordinates = {
     y: -ring,
 }
  
-//find the position of the ring the number is one
 let count = 0;
 while(sum != input){
-    //console.log(coordinates, count,lenghtOfSide,sum );
    
     if(count < lenghtOfSide-1)
         coordinates.x--;
